10e7fbbc75d9b95d4fd59f0659851bbc631a1f41
dimss113 <dimasfadilah20@gmail.com>
2023-03-28 20:36:56 +0700

    intial commit

--- js/main.js
+++ js/main.js
@@ -1,7 +1,49 @@
-/*
- * When viewport is less than 600px wide, a responsive menu pops up by clicking on hamburger icon
- *
- */
+$(document).ready(function () {
+  var delay = 2000;
+  $(".submit-btn").click(function (e) {
+    e.preventDefault();
+
+    var email = $("#email").val();
+    if (email == "") {
+      $(".message_box").html(
+        '<span style="color:red;">Enter Email Address!</span>'
+      );
+      $("#email").focus();
+      return false;
+    }
+    if ($("#email").val() != "") {
+      if (!isValidEmailAddress($("#email").val())) {
+        $(".message_box").html(
+          '<span style="color:red;">Provided email address is incorrect!</span>'
+        );
+        $("#email").focus();
+        return false;
+      }
+    }
+    $.ajax({
+      type: "POST",
+      url: "ajax.php",
+      data: "&email=" + email,
+      beforeSend: function () {
+        $(".message_box").html(
+          '<p>SUCCESS SENDING MESSAGE</p>'
+        );
+      },
+      success: function (data) {
+        setTimeout(function () {
+          $(".message_box").html(data);
+        }, delay);
+      },
+    });
+  });
+});
+
+function isValidEmailAddress(emailAddress) {
+  var pattern = /^([a-z\d!#$%&'*+\-\/=?^_`{|}~\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]+(\.[a-z\d!#$%&'*+\-\/=?^_`{|}~\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]+)*|"((([ \t]*\r\n)?[ \t]+)?([\x01-\x08\x0b\x0c\x0e-\x1f\x7f\x21\x23-\x5b\x5d-\x7e\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]|\\[\x01-\x09\x0b\x0c\x0d-\x7f\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]))*(([ \t]*\r\n)?[ \t]+)?")@(([a-z\d\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]|[a-z\d\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF][a-z\d\-._~\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]*[a-z\d\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])\.)+([a-z\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]|[a-z\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF][a-z\d\-._~\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]*[a-z\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])\.?$/i;
+  return pattern.test(emailAddress);
+}
+
+
 
 var menu = document.querySelector('.menu');
 var responsiveNav = document.querySelector("#drawer");
@@ -17,55 +59,4 @@ menu.addEventListener('click', function () {
 });
 
 
-( function($) {
-  
-  $(document).ready(function() {
-    
-    var s           = $('.slider'),
-        sWrapper    = s.find('.slider-wrapper'),
-        sItem       = s.find('.slide'),
-        btn         = s.find('.slider-link'),
-        sWidth      = sItem.width(),
-        sCount      = sItem.length,
-        slide_date  = s.find('.slide-date'),
-        slide_title = s.find('.slide-title'),
-        slide_text  = s.find('.slide-text'),
-        slide_more  = s.find('.slide-more'),
-        slide_image = s.find('.slide-image img'),
-        sTotalWidth = sCount * sWidth;
-    
-    sWrapper.css('width', sTotalWidth);
-    sWrapper.css('width', sTotalWidth);
-    
-    var clickCount  = 0;
-    
-    btn.on('click', function(e) {
-      e.preventDefault();
 
-      if( $(this).hasClass('next') ) {
-        
-        ( clickCount < ( sCount - 1 ) ) ? clickCount++ : clickCount = 0;
-      } else if ( $(this).hasClass('prev') ) {
-        
-        ( clickCount > 0 ) ? clickCount-- : ( clickCount = sCount - 1 );
-      }
-      TweenMax.to(sWrapper, 0.4, {x: '-' + ( sWidth * clickCount ) })
-
-
-      //CONTENT ANIMATIONS
-
-      var fromProperties = {autoAlpha:0, x:'-50', y:'-10'};
-      var toProperties = {autoAlpha:0.8, x:'0', y:'0'};
-
-      TweenLite.fromTo(slide_image, 1, {autoAlpha:0, y:'40'}, {autoAlpha:1, y:'0'});
-      TweenLite.fromTo(slide_date, 0.4, fromProperties, toProperties);
-      TweenLite.fromTo(slide_title, 0.6, fromProperties, toProperties);
-      TweenLite.fromTo(slide_text, 0.8, fromProperties, toProperties);
-      TweenLite.fromTo(slide_more, 1, fromProperties, toProperties);
-
-    });
-          
-  });
-})(jQuery);
-
-$('.overlay').addClass('overlay-blue');
